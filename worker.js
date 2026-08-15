92c00b586086ec5c4777709238c19f61a8a5cd21
Ye HtetAung <yehtetaung7655@gmail.com>
2026-08-15 17:31:30 +0630

    Add worker.js to handle fetch requests

---
+++ worker.js
@@ -0,0 +1,9 @@
+export default {
+  async fetch(request, env, ctx) {
+    return new Response("Brazil 2D is working 🇧🇷", {
+      headers: {
+        "Content-Type": "text/plain; charset=UTF-8",
+      },
+    });
+  },
+};
