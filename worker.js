7bfe455811702c0e34080a4e8c005a64fe9e0dd7
Ye HtetAung <yehtetaung7655@gmail.com>
2026-08-15 17:45:49 +0630

    Fix response message in worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request, env, ctx) {
-    return new Response("Brazil 2D is working 🇧🇷", {
+    return new Response("Brazil 2D is working! 🇧🇷", {
       headers: {
         "Content-Type": "text/plain; charset=UTF-8",
       },
